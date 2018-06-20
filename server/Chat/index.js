d4a3eab0fe5772ac54f69d5e64d076b3f650d25d
Daniel Sandoval <sandovaljda@gmail.com>
2018-06-19 22:14:36 -0500

    Integrando el front y back-end

--- server/Chat/index.js
+++ server/Chat/index.js
@@ -1,20 +1,67 @@
 var express = require('express')
+
+var Storage = require('../Storage')
+
 var Router = express.Router()
 
 Router.get('/users', function(req, res){
     //get Usuario
+    Storage.getData('users')    
+            .then(function(users){
+                res.json(users)
+            }).catch(function(error){
+                res.sendStatus(500).json(error)
+            })
 })
 
 Router.get('/messages', function(req, res){
     //get Mensajes
+    Storage.getData('message')    
+            .then(function(messages){
+                res.json(messages)
+            }).catch(function(error){
+                res.sendStatus(500).json(error)
+            })
 })
 
 Router.post('/users', function(req, res){
     //post Usuario
+    var user = req.body.users
+    Storage.getData('users')
+            .then(function(users){
+                return new Promise(function (resolve, reject){
+                    Storage.saveData('users', user, users)
+                            .then(function(message){
+                                resolve(message)
+                            }).catch(function(err){
+                                reject(err)
+                            })
+                })
+            }).then(function(message){
+                res.json(message)
+            }).catch(function(err){
+                res.sendStatus(500).json(err)
+            })
 })
 
 Router.post('/messages', function(req, res){
     //post Mensajes
+    var message = req.body.message
+    Storage.getData('messages')
+            .then(function(messages){
+                return new Promise(function (resolve, reject){
+                    Storage.saveData('users', message, messages)
+                            .then(function(message){
+                                resolve(message)
+                            }).catch(function(err){
+                                reject(err)
+                            })
+                })
+            }).then(function(message){
+                res.json(message)
+            }).catch(function(err){
+                res.sendStatus(500).json(err)
+            })
 })
 
 module.exports = Router
